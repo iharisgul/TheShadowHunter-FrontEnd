b595fbae85e0e9ec053aa745a6bbed935ea4ca81
iharisgul <49853418+iharisgul@users.noreply.github.com>
2020-07-18 16:21:45 +0500

    Update Monitoring.js

--- FrontEnd-UI/Monitoring.js
+++ FrontEnd-UI/Monitoring.js
@@ -1,217 +1,255 @@
-import React, { Component } from 'react';
-import './Monitoring.css';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Monitoring from './Monitoring.js';
+import React, { Component } from "react";
+import "./Monitoring.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Monitoring from "./Monitoring.js";
 
-import Signup from './Signup.js';
-import Dashboard from './Dashboard';
-import Navigation from './Navigation'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import Signup from "./Signup.js";
+import Dashboard from "./Dashboard";
+import Navigation from "./Navigation";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import moment from "moment";
-import NavigationManager from './NavigationManager'
-import NavigationSubManager from './NavigationSubManager'
-import axios from 'axios';
-
+import NavigationManager from "./NavigationManager";
+import NavigationSubManager from "./NavigationSubManager";
+import { axiosInstance } from "../common/config";
 
 class Monitorings extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            managers:null,
-            submanagers: null,
-            departments: null,
-            services: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      managers: null,
+      submanagers: null,
+      departments: null,
+      services: null,
+    };
+    //for department
+    this.getDepartments();
+    this.deleteDepartments = this.deleteDepartments.bind(this);
+    this.deld = this.deld.bind(this);
 
-
-
-        axios({
-            method: 'post',
-            url: 'http://localhost:5000/api/users/getdepartment'
-            
-        })
-            .then(response => {
-                  this.setState({departments: response.data.departments})
-                    
-            })
-            .catch(err => alert(err + 'Could not get departments'));
-
-            axios({
-              method: 'post',
-              url: 'http://localhost:5000/api/users/getservices',//'http://115.186.176.139:5000/api/attacksessions/startattacksession',
-              
-          })
-    
-        
-        .then(response => {
-              this.setState({services: response.data.services})
-                
-        })
-        .catch(err => alert(err + 'Could not get services'));
-
-
-    }
-
-   rowDiv(i){
-
-    if ((i)%4 === 0){
-    
-      return(
-         <div className='row'>
-           <div className="col s12 m12 l12">
-             </div>
-             </div>
-         )
-     
-       }
-      else{
-return(null)
-      }
-
-   } 
-
-   deleteService(e){
-    console.log(e.target.value)
-    
-    axios(
-      {
-      method: "post",
-      url:"http://192.168.1.110:8080/execute-VM",
-      data: {"ip_address":(e.target.value)}
-    }
-    ).then(res=>{
-      console.log(res.e.target.value);
-      //  updateStatus(res.data);
-    
-    }).catch(err=>{console.log(err)})
-    console.log(JSON.stringify(e.target.value))
-
-
+    //for services
+    this.getServices();
+    this.deleteServices = this.deleteServices.bind(this);
+    this.dels = this.dels.bind(this);
   }
-    
 
-deleteService(e){
-console.log(e.target.value)
+  // for departments
+  getDepartments() {
+    axiosInstance({
+      method: "post",
+      url: "/api/users/getdepartment",
+    })
+      .then((response) => {
+        this.setState({ departments: response.data.departments });
+      })
+      .catch((err) => alert(err + "Could not get departments"));
+  }
 
-axios(
-  {
-  method: "post",
-  url:"http://192.168.1.110:8080/execute-VM",
-  data: {"ip_address":(e.target.value)}
-}
-).then(res=>{
-  console.log(res.e.target.value);
-  //  updateStatus(res.data);
+  //for services
+  getServices() {
+    axiosInstance({
+      method: "post",
+      url: "/api/users/getservices",
+    })
+      .then((response) => {
+        this.setState({ services: response.data.services });
+      })
+      .catch((err) => alert(err + "Could not get services"));
+  }
 
-}).catch(err=>{console.log(err)})
-console.log(JSON.stringify(e.target.value))
+  //for department
+  deleteDepartments = async (_id) => {
+    await axiosInstance({
+      method: "post",
+      url: "/api/users/deleteDepartments",
+      data: { _id: _id },
+    })
+      .then((e) => {
+        console.log("Department has been permanantly deleted", "success");
+        console.log(_id)
+      })
+      .catch((err) => alert(err + "Could not delete Department"));
+  };
 
+  deld(e) {
+    e.preventDefault();
+    this.deleteDepartments(e.currentTarget.value);
+    console.log(e.currentTarget.value)
+    this.getDepartments();
+    window.location.reload(true);
+  }
 
+  //for services
 
+  deleteServices = async (_id) => {
+    await axiosInstance({
+      method: "post",
+      url: "/api/users/deleteServices",
+      data: { _id: _id },
+    })
+      .then((e) => {
+        console.log("VM has been permanantly deleted", "success");
+      })
+      .catch((err) => alert(err + "Could not delete VM"));
+  };
 
-}
-    
+  dels(e) {
+    e.preventDefault();
+    this.deleteServices(e.currentTarget.value);
+    this.getServices();
+    window.location.reload(true);
+  }
+
+  rowDiv(i) {
+    if (i % 4 === 0) {
+      return (
+        <div className="row">
+          <div className="col s12 m12 l12"></div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  //    deleteService(e){
+  //     console.log(e.target.value)
+
+  //     axiosInstance(
+  //       {
+  //       method: "post",
+  //       url:"http://192.168.1.110:8080/execute-VM",
+  //       data: {"ip_address":(e.target.value)}
+  //     }
+  //     ).then(res=>{
+  //       console.log(res.e.target.value);
+  //       //  updateStatus(res.data);
+
+  //     }).catch(err=>{console.log(err)})
+  //     console.log(JSON.stringify(e.target.value))
+
+  //   }
+
+  // deleteService(e){
+  // console.log(e.target.value)
+
+  // axiosInstance(
+  //   {
+  //   method: "post",
+  //   url:"http://192.168.1.110:8080/execute-VM",
+  //   data: {"ip_address":(e.target.value)}
+  // }
+  // ).then(res=>{
+  //   console.log(res.e.target.value);
+  //   //  updateStatus(res.data);
+
+  // }).catch(err=>{console.log(err)})
+  // console.log(JSON.stringify(e.target.value))
+
+  // }
+
   render() {
-    let cards =[]
-    let elements =[]
-    let vmcards=[]
+    let cards = [];
+    let elements = [];
+    let vmcards = [];
     return (
       <BrowserRouter>
-          <Route path='/navigation' component={Navigation} />
-        <div className='app'>
-        {this.props.role === "admin" ? <Navigation/>:(this.props.role === "manager" ? <NavigationManager/>:(this.props.role === "submanager"?<NavigationSubManager/>:null ) )}
+        <Route path="/navigation" component={Navigation} />
+        <div className="app">
+          {this.props.role === "admin" ? (
+            <Navigation />
+          ) : this.props.role === "manager" ? (
+            <NavigationManager />
+          ) : this.props.role === "submanager" ? (
+            <NavigationSubManager />
+          ) : null}
           {/* <!-- Navbar goes here --> */}
-         
 
-          <div className='row'>
-    
-
-            <div className='col s12 m8 l9 offset-l2 main' style={{marginLeft:'18%', marginRight:"1%"}}>
+          <div className="row">
+            <div
+              className="col s12 m8 l9 offset-l2 main"
+              style={{ marginLeft: "18%", marginRight: "1%" }}
+            >
               {/* <!-- Note that "m8 l9" was added --> */}
               {/* <!-- Teal page content */}
 
-    {/* <div className="row" > */}
-         {
-          
-              
-            this.state.departments ? this.state.departments.map((fields, i)=>
-            {
-             elements.push(
-               
-                <div key={fields.IPaddress} className="col s12 m4 l4">
-                              <div className='card text-black o-hidden white h-100' >
-                              <i class="large material-icons center">desktop_windows</i>
-                                    <div className='card-body'>
-                              <h2 className="center">Department</h2>
-                              <hr></hr>
-                              <h4>IP Address: {fields.IPaddress}</h4>
-                              <hr></hr>
-                              <h6>Account used: {fields.account}</h6>
-                              <hr></hr>
-                              <h6>Account password: {fields.password}</h6>
-                              <hr></hr>
-                              <h6>{moment (new Date(fields.date)).format('DD/MM/YYYY , hh:mm:ss:A')}</h6>
-                              <hr></hr>
-                              <button class="btn btn-danger"value={JSON.stringify(fields)}  onClick={e=>this.deleteService(e)}>
-                   Delete
-                  </button>
+              {/* <div className="row" > */}
+              {this.state.departments
+                ? this.state.departments.map((fields, i) => {
+                    const { IPaddress, account, password, date, _id } = fields;
+                    elements.push(
+                      <div key={IPaddress} className="col s12 m4 l4">
+                        <div className="card text-black o-hidden white h-100">
+                          <i class="large material-icons center">
+                            desktop_windows
+                          </i>
+                          <div className="card-body">
+                            <h2 className="center">Department</h2>
+                            <hr></hr>
+                            <h4>IP Address: {IPaddress}</h4>
+                            <hr></hr>
+                            <h6>Account used: {account}</h6>
+                            <hr></hr>
+                            <h6>Account password: {password}</h6>
+                            <hr></hr>
+                            <h6>
+                              {moment(new Date(date)).format(
+                                "DD/MM/YYYY , hh:mm:ss:A"
+                              )}
+                            </h6>
+                            <hr></hr>
+                            <button
+                              class="btn btn-danger"
+                              value={_id}
+                              onClick={(e) => this.deld(e)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    );
 
-                </div>
-                </div>           
-                              </div>)
-                              
-                  
-                  if ((i+1)%3 === 0){
-    
-                   cards.push( <div className ="row">
-                   {elements}
-                 </div>)
-                   elements=[]
-                   
-                   
-                     }
-                     if (i+1 === this.state.departments.length){
-                      if(elements.length >0 ){
-                        console.log(elements)
-                          cards.push( <div className ="row">
-                          {elements}
-                        </div>)
-                          elements=[]
-                     }
-            }
-          }
-           ) 
-          : null
-        }
-        {/* dalta pori ba copy ke */}
+                    if ((i + 1) % 3 === 0) {
+                      cards.push(<div className="row">{elements}</div>);
+                      elements = [];
+                    }
+                    if (i + 1 === this.state.departments.length) {
+                      if (elements.length > 0) {
+                        console.log(elements);
+                        cards.push(<div className="row">{elements}</div>);
+                        elements = [];
+                      }
+                    }
+                  })
+                : null}
+              {/* dalta pori ba copy ke */}
 
-        
-
-        {
-           
-              
-          this.state.services ? this.state.services.map((fields, i)=>
-          {
-           elements.push(
-             
-              <div key={fields.IPaddress} className="col s12 m4 l4">
-                <hr></hr>
-                            <div className='card text-black o-hidden white h-100' >
-                            <i class="large material-icons center">desktop_mac</i>
-                                  <div className='card-body'>
+              {this.state.services
+                ? this.state.services.map((fields, i) => {
+                  const { IPaddress, account, password, service, date, _id } = fields;
+                    elements.push(
+                      <div key={fields.IPaddress} className="col s12 m4 l4">
+                        <hr></hr>
+                        <div className="card text-black o-hidden white h-100">
+                          <i class="large material-icons center">desktop_mac</i>
+                          <div className="card-body">
                             <h2 className="center">VM</h2>
                             <hr></hr>
-                            <h4>IP Address: {fields.IPaddress}</h4>
-                            
+                            <h4>IP Address: {IPaddress}</h4>
+
                             <hr></hr>
-                            <h6>Account used: {fields.account}</h6>
+                            <h6>Account used: {account}</h6>
                             <hr></hr>
-                            <h6>Account password: {fields.password}</h6>
+                            <h6>Account password: {password}</h6>
                             <hr></hr>
-                            <h6>VM up: {fields.service}</h6>
+                            <h6>VM up: {service}</h6>
                             <hr></hr>
-                            <h6>{moment (new Date(fields.date)).format('DD/MM/YYYY , hh:mm:ss:A')}</h6>
+                            <h6>
+                              {moment(new Date(date)).format(
+                                "DD/MM/YYYY , hh:mm:ss:A"
+                              )}
+                            </h6>
                             <hr></hr>
                             {/* <div className="led-box" style={{marginLeft:'37%'}}>
                         <div className="led-green" > </div>
@@ -226,48 +264,37 @@ console.log(JSON.stringify(e.target.value))
     
                         </div>
                                  <hr></hr> */}
-                            <button class="btn btn-danger"value={JSON.stringify(fields)}  onClick={e=>this.deleteService(e)}>
-                 Delete
-                </button>
+                            <button
+                              class="btn btn-danger"
+                              value={_id}
+                              onClick={(e) => this.dels(e)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    );
 
-              </div>
-              </div>           
-                            </div>)
-                
-                if ((i+1)%3 === 0){
-  
-                 vmcards.push( <div className ="row">
-                 {elements}
-               </div>)
-                 elements=[]
-                 
-                 
-                   }
-                   if (i+1 === this.state.services.length){
-                    if(elements.length >0 ){
-                      console.log(elements)
-                        vmcards.push( <div className ="row">
-                        {elements}
-                      </div>)
-                        elements=[]
-                   }
-          }
-        }
-         ) 
-        : null
-      }
+                    if ((i + 1) % 3 === 0) {
+                      vmcards.push(<div className="row">{elements}</div>);
+                      elements = [];
+                    }
+                    if (i + 1 === this.state.services.length) {
+                      if (elements.length > 0) {
+                        console.log(elements);
+                        vmcards.push(<div className="row">{elements}</div>);
+                        elements = [];
+                      }
+                    }
+                  })
+                : null}
 
-
-
-
-    {  cards }
-    {  vmcards }
-        </div>
-
-                
-              </div>
-           
+              {cards}
+              {vmcards}
+            </div>
           </div>
+        </div>
       </BrowserRouter>
     );
   }
@@ -277,11 +304,12 @@ Monitorings.propTypes = {
   isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   token: state.auth.token,
-  role: state.auth.role
-})
+  role: state.auth.role,
+});
 
-export default connect(mapStateToProps)(Monitorings)
+export default connect(mapStateToProps)(Monitorings);
 
+//value={JSON.stringify(fields),}  onClick={e=>this.deleteService(e)}
